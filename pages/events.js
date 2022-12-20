71a0f051530c78841d4cb9fe96b152dda28188c4
Aaron Eveleth <aaroneveleth@Aarons-MacBook-Pro.local>
2022-12-20 16:25:24 -0500

    add event page

---
+++ pages/events.js
@@ -0,0 +1,17 @@
+import { Container, Grid } from "@mui/material";
+import Head from 'next/head';
+
+export default function Index({
+}) {
+  return (
+    <Container disableGutters maxWidth={false}>
+      <Head>
+      </Head>
+      <Grid container sx={{ display: "flex" }}>
+        <Grid item sx={{ width: "100%" }}>
+            <h1>Events page</h1>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
