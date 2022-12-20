59b065e79529c1e4f70df6fbdd8677f66e9e9eec
Aaron Eveleth <aaroneveleth@Aarons-MacBook-Pro.local>
2022-12-20 16:28:04 -0500

    add router for mobile nav buttons

--- components/AppDrawer/AppDrawer.jsx
+++ components/AppDrawer/AppDrawer.jsx
@@ -9,6 +9,7 @@ import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import { useRouter } from 'next/router';
 
 const drawerBleeding = 56;
 
